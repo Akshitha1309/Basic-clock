184beef160af12c3a3af71bfd93d70d5e77a4365
Akshitha1309 <139578978+Akshitha1309@users.noreply.github.com>
2024-02-17 15:05:18 +0530

    Updated format for hours and minutes

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ setInterval(() => {
     let minutes = temp.getMinutes();
     let seconds = temp.getSeconds();
     
+    // Adding leading zeros if hours is less than 10
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    
+    // Adding leading zeros if minutes is less than 10
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    
     // Adding leading zeros if seconds is less than 10
     if (seconds < 10) {
         seconds = '0' + seconds;
